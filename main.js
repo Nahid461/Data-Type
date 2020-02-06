0213786f201fb3136c0577d44a59072aecc6c46e
Nahid fatima <fatimanahid04@gmail.com>
2020-02-06 12:45:56 +0530

    #Nahid Fatima: Data Type Define

---
+++ main.js
@@ -0,0 +1,85 @@
+//Data Type
+
+
+//Number Data Type 
+let numValue = 10;//Declaration of variable
+ console.log(numValue);// 10
+console.log(typeof(numValue)); //Number
+
+numValue = "Nahid";
+console.log(numValue); //Nahid
+console.log(typeof(numValue));//string
+
+
+//string Data type
+
+let strValue = "Javascript";
+console.log(strValue);//Javascript
+console.log(typeof(strValue));//string
+
+//Boolean Data Type
+
+let boolValue=false;//false
+console.log(boolValue);
+console.log(typeof(boolValue));
+
+//Reference Type
+
+//Array
+ 
+ let arrayObj = [1,2,3,4,5];{
+ for(let counter=0, counter<=4  arrayObj.length-1, counter++)// old syntex used when data coming in bulk
+ {
+    
+   console.log(counter);
+    
+}}
+
+for(let numValue2 of arrayObj )//foreach syntex used mostly in javascript
+{
+console.log(numValue2); 
+
+}
+let arrayObj1 = [];
+arrayObj1[0]=1;
+arrayObj1[1]=2;
+arrayObj1[5]=3;
+
+
+ 
+
+
+
+
+
+arrayObj1[5]=5;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
